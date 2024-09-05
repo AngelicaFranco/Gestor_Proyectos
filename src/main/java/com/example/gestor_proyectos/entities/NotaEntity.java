359a76caf3044d9cef1b9461ae27802bcb230aec
AngelicaFranco <angelicafranco@utp.edu.co>
2024-09-05 17:57:04 -0500

    Creacion clases y atributos de las entidades

--- src/main/java/com/example/gestor_proyectos/entities/NotaEntity.java
+++ src/main/java/com/example/gestor_proyectos/entities/NotaEntity.java
@@ -1,4 +1,24 @@
 package com.example.gestor_proyectos.entities;
 
+import com.fasterxml.jackson.databind.DatabindException;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "notas")
+
 public class NotaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "fecha_realizacion")
+    private Date fechaRealizacion;
+
+    @Column(name = "nota")
+    private String nota;
 }
--- src/main/java/com/example/gestor_proyectos/entities/TareaEntity.java
+++ src/main/java/com/example/gestor_proyectos/entities/TareaEntity.java
@@ -1,4 +1,29 @@
 package com.example.gestor_proyectos.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tarea")
+
 public class TareaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "peso")
+    private Integer peso;
+
+    @Column(name = "porcentaje_cumplimiento")
+    private Integer porcentajeCumplimiento;
+
+
 }
